1328160ea49f54f7092e3383a62038b9e50b876d
will <willmiller629@gmail.com>
2021-04-25 17:55:56 -0400

    oop basics

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+namespace GradeBook
+{
+    public class Book
+    {
+
+        private List<double> grades = new List<double>();
+        private string name;
+
+        public Book(String name)
+        {
+            this.name = name;
+            grades = new List<double>();
+        }
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach (var grade in grades)
+            {
+                lowGrade = Math.Min(grade, lowGrade);
+                highGrade = Math.Max(grade, highGrade);
+
+                result += grade;
+            }
+
+            result = result / grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
+            System.Console.WriteLine($"The average result is {result:N1}");
+
+
+        }
+
+
+    }
+}
---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var book = new Book("Main Gradebook");
+            book.AddGrade(81.6);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            book.ShowStatistics();
+
+    
+        }
+    }
+}
